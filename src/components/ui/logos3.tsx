25b8def346f7d0ce7e447d6110dc18dd819cdf8f
thefly666 <133391109+thefly666@users.noreply.github.com>
2025-05-17 10:11:20 -0400

    Update logos with proper imports

--- src/components/ui/logos3.tsx
+++ src/components/ui/logos3.tsx
@@ -1,34 +1,37 @@
-import { cn } from "@/lib/utils"
+import logoAuthentica from '/images/logos/logo_authentica.png'
+import logoCodeVerify from '/images/logos/logo_codeverify.png'
+import logoHometrics from '/images/logos/logo_hometrics.png'
+import logoLeadLink from '/images/logos/logo_leadlink.png'
+import logoTrendSwap from '/images/logos/logo_trendswap.png'
+import logoWanted from '/images/logos/logo_wanted.png'
+import logoYouTubeCondensed from '/images/logos/logo_youtubecondensed.png'
+import logoZapAI from '/images/logos/logo_zapai.png'
 
 export function Logos3() {
   return (
-    <div className="relative flex flex-col items-center justify-center overflow-hidden bg-black py-20">
-      <div className="w-full flex-none">
-        <div className="relative w-full overflow-hidden">
-          <div className="pointer-events-none relative flex gap-6 py-4">
-            <div className="animate-scroll flex min-w-full shrink-0 items-center justify-around gap-6">
-              <img src="/images/logos/logo_authentica.png" alt="Authentica" className="h-8 w-auto object-contain" />
-              <img src="/images/logos/logo_codeverify.png" alt="CodeVerify" className="h-8 w-auto object-contain" />
-              <img src="/images/logos/logo_hometrics.png" alt="Hometrics" className="h-8 w-auto object-contain" />
-              <img src="/images/logos/logo_leadlink.png" alt="LeadLink" className="h-8 w-auto object-contain" />
-              <img src="/images/logos/logo_trendswap.png" alt="TrendSwap" className="h-8 w-auto object-contain" />
-              <img src="/images/logos/logo_wanted.png" alt="Wanted" className="h-8 w-auto object-contain" />
-              <img src="/images/logos/logo_youtubecondensed.png" alt="YouTube Condensed" className="h-8 w-auto object-contain" />
-              <img src="/images/logos/logo_zapai.png" alt="ZapAI" className="h-8 w-auto object-contain" />
-            </div>
-            <div aria-hidden="true" className="animate-scroll flex min-w-full shrink-0 items-center justify-around gap-6">
-              <img src="/images/logos/logo_authentica.png" alt="Authentica" className="h-8 w-auto object-contain" />
-              <img src="/images/logos/logo_codeverify.png" alt="CodeVerify" className="h-8 w-auto object-contain" />
-              <img src="/images/logos/logo_hometrics.png" alt="Hometrics" className="h-8 w-auto object-contain" />
-              <img src="/images/logos/logo_leadlink.png" alt="LeadLink" className="h-8 w-auto object-contain" />
-              <img src="/images/logos/logo_trendswap.png" alt="TrendSwap" className="h-8 w-auto object-contain" />
-              <img src="/images/logos/logo_wanted.png" alt="Wanted" className="h-8 w-auto object-contain" />
-              <img src="/images/logos/logo_youtubecondensed.png" alt="YouTube Condensed" className="h-8 w-auto object-contain" />
-              <img src="/images/logos/logo_zapai.png" alt="ZapAI" className="h-8 w-auto object-contain" />
-            </div>
+    <div className="w-full bg-black py-16">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="relative flex overflow-x-hidden">
+          <div className="animate-scroll flex space-x-16 py-12">
+            <img src={logoAuthentica} alt="Authentica" className="max-h-12 w-auto object-contain" />
+            <img src={logoCodeVerify} alt="CodeVerify" className="max-h-12 w-auto object-contain" />
+            <img src={logoHometrics} alt="Hometrics" className="max-h-12 w-auto object-contain" />
+            <img src={logoLeadLink} alt="LeadLink" className="max-h-12 w-auto object-contain" />
+            <img src={logoTrendSwap} alt="TrendSwap" className="max-h-12 w-auto object-contain" />
+            <img src={logoWanted} alt="Wanted" className="max-h-12 w-auto object-contain" />
+            <img src={logoYouTubeCondensed} alt="YouTubeCondensed" className="max-h-12 w-auto object-contain" />
+            <img src={logoZapAI} alt="ZapAI" className="max-h-12 w-auto object-contain" />
           </div>
-          <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-black" />
-          <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-black" />
+          <div className="animate-scroll flex space-x-16 py-12" aria-hidden="true">
+            <img src={logoAuthentica} alt="Authentica" className="max-h-12 w-auto object-contain" />
+            <img src={logoCodeVerify} alt="CodeVerify" className="max-h-12 w-auto object-contain" />
+            <img src={logoHometrics} alt="Hometrics" className="max-h-12 w-auto object-contain" />
+            <img src={logoLeadLink} alt="LeadLink" className="max-h-12 w-auto object-contain" />
+            <img src={logoTrendSwap} alt="TrendSwap" className="max-h-12 w-auto object-contain" />
+            <img src={logoWanted} alt="Wanted" className="max-h-12 w-auto object-contain" />
+            <img src={logoYouTubeCondensed} alt="YouTubeCondensed" className="max-h-12 w-auto object-contain" />
+            <img src={logoZapAI} alt="ZapAI" className="max-h-12 w-auto object-contain" />
+          </div>
         </div>
       </div>
     </div>
