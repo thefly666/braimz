9b6707e734d8935a034b108d13515af984dc98e9
thefly666 <133391109+thefly666@users.noreply.github.com>
2025-05-22 17:34:16 -0400

    Update navigation menu

--- src/components/NavBarDemo.tsx
+++ src/components/NavBarDemo.tsx
@@ -3,10 +3,10 @@ import { NavBar } from './ui/tubelight-navbar'
 
 export function NavBarDemo() {
   const navItems = [
+    { name: 'Home', url: '#', icon: Home },
     { name: 'About', url: '#about', icon: User },
     { name: 'Our Services', url: '#services', icon: Briefcase },
     { name: 'Pricing', url: '#pricing', icon: CreditCard }
   ]
 
   return <NavBar items={navItems} />
-}
